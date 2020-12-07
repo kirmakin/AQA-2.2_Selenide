70a1f4f82323dddd8cba5d4f0e066eb127583493
Kirill <kiruxastar@gmail.com>
2020-12-07 16:53:22 +0300

    New test added

--- src/test/java/ru/netology/WebpageTest.java
+++ src/test/java/ru/netology/WebpageTest.java
@@ -25,14 +25,26 @@ public class WebpageTest {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999/");
-        $("[data-test-id=city] .input__control").setValue("Мо");
-        $$(".menu-item__control").findBy(text("Москва")).click();
-        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false), Keys.CONTROL + "A", Keys.DELETE);
-        $$(".calendar__day").findBy(text(when(true))).click();
+        $("[data-test-id=city] .input__control").setValue("Москва");
+        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false));
         $("[data-test-id=name] [name=name]").setValue("Иван Сидоров");
         $("[data-test-id=phone] [name=phone]").setValue("+79200000011");
         $("[data-test-id=agreement]>.checkbox__box").click();
         $("button>.button__content").click();
         $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + when(false)));
     }
+
+        @Test
+        void shouldSubmitComplexElementRequest () {
+            open("http://localhost:9999/");
+            $("[data-test-id=city] .input__control").setValue("Мо");
+            $$(".menu-item__control").findBy(text("Москва")).click();
+            $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false), Keys.CONTROL + "A", Keys.DELETE);
+            $$(".calendar__day").findBy(text(when(true))).click();
+            $("[data-test-id=name] [name=name]").setValue("Иван Сидоров");
+            $("[data-test-id=phone] [name=phone]").setValue("+79200000011");
+            $("[data-test-id=agreement]>.checkbox__box").click();
+            $("button>.button__content").click();
+            $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + when(false)));
+        }
 }
